2092f6d28b95dfd20b8d19fd8056e5367381fe4c
Koerner Gray Buchta <koerner@Koerners-MacBook-Pro-2.local>
2025-05-31 16:20:03 -0400

    Remove env directory from git tracking

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, url_for, request
-from flask.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
